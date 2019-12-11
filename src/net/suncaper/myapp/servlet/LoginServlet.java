dc2c118ad576b6f1117392b0d27391c1fe379a12
smallDanDan <1209295751@qq.com>
2019-12-11 18:00:04 +0800

    12.11 serverlet搭建

--- src/net/suncaper/myapp/servlet/LoginServlet.java
+++ src/net/suncaper/myapp/servlet/LoginServlet.java
@@ -23,7 +23,21 @@ public class LoginServlet extends HttpServlet {
             request.getRequestDispatcher("/views/login.jsp").forward(request,response);
         }
 
+        // userName = new String(userName.getBytes("ISO-8859-1"),"UTF-8");//字符转码的方式
+   /*    System.out.println(userName+"/"+password);
 
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter writer = response.getWriter();
+        writer.println("已经接受daoshuju");*/
+
+/**
+ request 的作用
+
+ 1. 获取浏览器发送的参数
+ 2. 转发到其他路径
+ 3. 可以存储属性值,相当于Map,(存储数据,交给页面作展示)
+ */
 
     }
 }
